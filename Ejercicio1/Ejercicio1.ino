0c86704f51e79b6cf3b18ab892dcf47bd752c8e0
Kenai <49519358@est.ort.edu.ar>
2026-06-18 17:21:17 -0300

    cambie logica de pasaje de pantallas

--- Ejercicio1/Ejercicio1.ino
+++ Ejercicio1/Ejercicio1.ino
@@ -16,8 +16,7 @@ Preferences preferences;
 DHT dht(DHTPIN, DHTTYPE);
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
 
-typedef enum
-{
+typedef enum {
   RST,
   P1,
   P2
@@ -28,20 +27,19 @@ estados_t maquinaPantalla = RST;
 float temperatura = 0;
 float valorUmbral = 26;
 
+bool sw1_flag = false;
+bool sw2_flag = false;
 unsigned long tiempo_sw1 = 0;
 unsigned long tiempo_sw2 = 0;
-bool sw1_anterior = false;
-bool sw2_anterior = false;
 long int millisUltimoCheck = 0;
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   pinMode(SW1, INPUT);
   pinMode(SW2, INPUT);
   pinMode(LEDPIN, OUTPUT);
   digitalWrite(LEDPIN, LOW);
-  
+
   dht.begin();
   u8g2.begin();
 
@@ -49,98 +47,92 @@ void setup()
   valorUmbral = preferences.getFloat("umbral", 26.0);
 }
 
-void loop()
-{
+void loop() {
+  bool sw1_actual = !digitalRead(SW1);
+  bool sw2_actual = !digitalRead(SW2);
 
   // Leer el sensor de temperatura cada 2 segundos
-  if (millis() - millisUltimoCheck >= 2000)
-  { 
+  if (millis() - millisUltimoCheck >= 2000) {
     float temperaturaTest = dht.readTemperature();
-    if (!isnan(temperaturaTest))
-    {
+    if (!isnan(temperaturaTest)) {
       temperatura = temperaturaTest;
     }
     millisUltimoCheck = millis();
   }
 
-  // Estado actual de los pulsadores (lógica negada)
-  bool sw1 = !digitalRead(SW1); 
-  bool sw2 = !digitalRead(SW2);
+  switch (maquinaPantalla) {
+    case RST:
+      maquinaPantalla = P1;
+      break;
 
-  // Variables de flanco (comparando el estado actual con el anterior)
-  bool sw1_flanco_subida = (sw1 && !sw1_anterior);
-  bool sw1_flanco_bajada = (!sw1 && sw1_anterior);
-  
-  bool sw2_flanco_subida = (sw2 && !sw2_anterior);
-  bool sw2_flanco_bajada = (!sw2 && sw2_anterior);
+    case P1:
+      u8g2.clearBuffer();
+      u8g2.setFont(u8g2_font_ncenB08_tr);
+      u8g2.setCursor(0, 15);
+      u8g2.print("VA : ");
+      u8g2.print(temperatura);
+      u8g2.setCursor(0, 35);
+      u8g2.print("VU : ");
+      u8g2.print(valorUmbral);
+      u8g2.sendBuffer();
 
-  switch (maquinaPantalla)
-  {
-  case RST:
-    maquinaPantalla = P1;
-    break;
-
-  case P1:
-    u8g2.clearBuffer();
-    u8g2.setFont(u8g2_font_ncenB08_tr);
-    u8g2.setCursor(0, 15);
-    u8g2.print("VA : ");
-    u8g2.print(temperatura);
-    u8g2.setCursor(0, 35);
-    u8g2.print("VU : ");
-    u8g2.print(valorUmbral);
-    u8g2.sendBuffer();
-
-    if (sw1_flanco_subida) {
-      tiempo_sw1 = millis();
-    }
-    
-    // Si se mantiene presionado
-    if (sw1) {
-      if (millis() - tiempo_sw1 >= 5000) {
-        maquinaPantalla = P2;
-        tiempo_sw1 = millis(); // Resetear para evitar transiciones erráticas
+      if (sw1_actual && !sw1_flag) {
+        tiempo_sw1 = millis();
+        sw1_flag = true;
       }
-    }
-    break;
 
-  case P2:
-    u8g2.clearBuffer();
-    u8g2.setFont(u8g2_font_ncenB08_tr);
-    u8g2.setCursor(0, 15);
-    u8g2.print("VU : ");
-    u8g2.print(valorUmbral);
-    u8g2.sendBuffer();
+      // Si se mantiene presionado
+      if (sw1_actual) {
+        if (millis() - tiempo_sw1 >= 5000) {
+          maquinaPantalla = P2;
+          tiempo_sw1 = millis();
+        }
+      } else sw1_flag = false;
 
-    // Aumentar umbral 1 grado al presionar SW1
-    if (sw1_flanco_subida) {
-      valorUmbral++;
-    }
+      break;
 
-    // Lógica para SW2: Disminuir o Volver
-    if (sw2_flanco_subida) {
-      tiempo_sw2 = millis();
-    }
-    
-    if (sw2) {
-      if (millis() - tiempo_sw2 >= 5000) {
-        // Volver a P1 y guardar en Preferences
-        maquinaPantalla = P1;
-        preferences.putFloat("umbral", valorUmbral);
-        Serial.print("Valor umbral guardado en memoria: ");
-        Serial.println(valorUmbral);
-        
-        // Anular el timer para no restar al soltar
-        tiempo_sw2 = 0; 
+    case P2:
+      u8g2.clearBuffer();
+      u8g2.setFont(u8g2_font_ncenB08_tr);
+      u8g2.setCursor(0, 15);
+      u8g2.print("VU : ");
+      u8g2.print(valorUmbral);
+      u8g2.sendBuffer();
+
+      if (sw1_actual) sw1_flag = true;
+      // Aumentar umbral 1 grado al presionar SW1
+      if (!sw1_actual && sw1_flag) {
+        valorUmbral++;
+        sw1_flag = false;
       }
-    } else if (sw2_flanco_bajada) {
-      // Se soltó SW2 antes de los 5 segundos, entonces disminuimos 1 grado
-      if (tiempo_sw2 != 0 && (millis() - tiempo_sw2 < 5000)) {
-        valorUmbral--;
+
+      // Lógica para SW2: Disminuir o Volver
+      if (sw2_actual && !sw2_flag) {
+        tiempo_sw2 = millis();
+        sw2_flag = true;
       }
+
+      if (sw2_actual) {
+        if (millis() - tiempo_sw2 >= 5000) {
+          // Volver a P1 y guardar en Preferences
+          maquinaPantalla = P1;
+          preferences.putFloat("umbral", valorUmbral);
+          Serial.print("Valor umbral guardado en memoria: ");
+          Serial.println(valorUmbral);
+
+          sw2_flag = false;
+          tiempo_sw2 = 0;
+        }
+      } else {
+          sw2_flag = false;
+          if (tiempo_sw2 != 0) {
+            valorUmbral--;
+            tiempo_sw2 = 0;
+          }
+        }
     }
 
-    break;
+      break;
   }
 
   // Guardar el estado actual como anterior para la próxima iteración
